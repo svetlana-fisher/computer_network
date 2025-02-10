807c9981ffbbb174a15a957d29ea249579271286
Artemis37 <77227982+svetlana-fisher@users.noreply.github.com>
2025-02-10 17:27:04 +0700

    task1 ping programm

---
+++ task1/task1.py
@@ -0,0 +1,24 @@
+from ping3 import ping
+import csv
+
+hosts = ["yandex.ru",
+         "google.com",
+         "github.com",
+         "stackoverflow.com",
+         "youtube.com",
+         "duckduckgo.com",
+         "wildberris.ru",
+         "ozon.ru",
+         "aliexpress.ru",
+         "forest-home.ru"]
+
+with open("pings.csv", "w", newline="") as file:
+    writer = csv.DictWriter(file, ["Address", "rtt (ms)"])
+    writer.writeheader()
+
+    for host in hosts:
+        rtt = ping(host)
+        if rtt is not None:
+            writer.writerow({"Address": host, "rtt (ms)": rtt * 1000})
+        else:
+            writer.writerow(({"Address": host, "rtt (ms)": "timeout"}))
